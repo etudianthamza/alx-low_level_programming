d07b2b4de2bf8993884f47fadb9a6731b3352cb5
Hamza <hamzabouhouch273@gmail.com>
2023-09-07 18:42:47 -0700

    u

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C progresse that prints with put function
+ *
+ * Return: Alawys 0 (Succes)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
