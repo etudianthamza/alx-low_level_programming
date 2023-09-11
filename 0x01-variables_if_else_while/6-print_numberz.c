7b4a40546de4beb34a22ca3e4532b5f8dec780e5
Hamza <hamzabouhouch273@gmail.com>
2023-09-11 14:57:21 -0700

    u

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ *
+ * Return: Alawys 0 (Succes)
+*/
+
+int main(void)
+{
+	int ch = 0;
+
+	while (ch <= 9)
+	{
+		putchar(ch);
+		ch++;
+	}
+	printf("\n");
+	return (0);
+}
