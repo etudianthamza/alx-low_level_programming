a53c3c4c7f9e7f504b85227e81eb043fac86e224
Hamza <hamzabouhouch273@gmail.com>
2023-09-11 17:02:03 -0700

    u

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,14 +9,12 @@
 
 int main(void)
 {
-	int i = 0;
-
-	for (; i <= 9; i++)
+	for (int i = 0; i <= 9; i++)
 	{
 		for (int j = i + 1; j <= 9; j++)
 		{
 			putchar('0' + i);
-			putcher('0' + j);
+			putchar('0' + j);
 			if (i < 8 || j < 9)
 			{
 				putchar(',');
