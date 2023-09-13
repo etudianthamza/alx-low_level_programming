f387b1a3725784782ba8ff77db1f9c86324c701f
Hamza <hamzabouhouch273@gmail.com>
2023-09-12 17:11:51 -0700

    i

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ *
+ * Return: Alawys 0 (Succes)
+*/
+
+int main(void)
+{
+	print_alphabet(void);
+	return (0);
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,9 +7,28 @@
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
+ *
 */
 
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
+}
+/**
+ * print_alphabet - write the alphabets in lowercase
+ * the alphabets to print
+ *
+ * Return: on success 1.
+*/
+void print_alphabet(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
 }
