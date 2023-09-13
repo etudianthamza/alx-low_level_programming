4adc2846102826ab9895e573d8f75fb1bb76c376
Hamza <hamzabouhouch273@gmail.com>
2023-09-12 17:55:49 -0700

    y

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - write the alphabets in lowercase
+ *
+ * the alphabets to print
+ *
+ * Return: on success 1.
+*/
+
+void print_alphabet(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
