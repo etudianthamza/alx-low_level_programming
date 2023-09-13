a421405266c4a392307cc8fe975e5ed7703c2e33
Hamza <hamzabouhouch273@gmail.com>
2023-09-13 07:10:39 -0700

    t

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - write the alphabets in lowercase
+ *
+ * @int : the alphabets to print
+ *
+ * Return: on success 1.
+*/
+
+int _abs(int)
+{
+	int i;
+
+	return (sqrt(i * i));
+}
