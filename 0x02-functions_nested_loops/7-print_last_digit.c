b943b52990f020527a3f1e9d06efa93088931c2e
Hamza <hamzabouhouch273@gmail.com>
2023-09-13 11:34:45 -0700

    u

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit- print the last digit of a number given
+ *
+ * @u : enter the number u for extract the last digit
+ *
+ * Return: Alawys 0 (Succes)
+ *
+*/
+
+int print_last_digit(int u)
+{
+	return (u % 10);
+}
