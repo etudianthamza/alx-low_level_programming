d296661a681c4f3bc91b9d80a60b90c7d1a0168c
Hamza <hamzabouhouch273@gmail.com>
2023-09-13 15:49:07 -0700

    g

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,66 +1,25 @@
 #include "main.h"
 
 /**
- * jack_bauer - write the alphabets in lowercase
- *
- *  the alphabets to print
+ * jack_bauer - Prints every minute of the day of Jack Bauer
  *
  * Return: on success 1.
 */
 
 void jack_bauer(void)
 {
-	int h = 0;
+	int hour, minute;
 
-	for (; h < 24 ; h++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		if (h < 10)
+		for (minute = 0; minute < 60; minute++)
 		{
-			int s = 0;
-
-			for (; s < 60 ; s++)
-			{
-				if (s < 10)
-				{
-					_putchar('0');
-					_putchar('0' + h);
-					_putchar(':');
-					_putchar('0');
-					_putchar('0' + s);
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar('0');
-					_putchar('0' + h);
-					_putchar(':');
-					_putchar('0' + s);
-					_putchar('\n');
-				}
-			}
-		}
-		else
-		{
-			int s = 0;
-
-			for (; s < 60 ; s++)
-			{
-				if (s < 10)
-				{
-					_putchar('0' + h);
-					_putchar(':');
-					_putchar('0');
-					_putchar('0' + s);
-					_putchar('\n');
-				}
-				else
-				{
-					_putchar('0' + h);
-					_putchar(':');
-					_putchar('0' + s);
-					_putchar('\n');
-				}
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
