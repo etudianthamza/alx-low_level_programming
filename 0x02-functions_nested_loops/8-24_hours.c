444de8ba67a46fceb8a34f4bbc90597cc9552e55
Hamza <hamzabouhouch273@gmail.com>
2023-09-13 15:22:45 -0700

    r

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,53 +10,53 @@
 
 void jack_bauer(void)
 {
-	int h = 0;
-
-	while (h < 10)
+	for (int h = 0; h < 24 ; h++)
 	{
-		int s = 0;
-
-		for (; s < 60 ; s++)
+		if (h < 10)
 		{
-			if (s < 10)
+			for (int s = 0; s < 60 ; s++)
 			{
-				_putchar('0');
-				_putchar('0' + h);
-				_putchar(':');
-				_putchar('0');
-				_putchar('0' + s);
-				_putchar('\n');
-			}
-			else
-			{
-				_putchar('0');
-				_putchar('0' + h);
-				_putchar(':');
-				_putchar('0' + s);
-				_putchar('\n');		
+				if (s < 10)
+				{
+					_putchar('0');
+					_putchar('0' + h);
+					_putchar(':');
+					_putchar('0');
+					_putchar('0' + s);
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar('0');
+					_putchar('0' + h);
+					_putchar(':');
+					_putchar('0' + s);
+					_putchar('\n');
+				}
 			}
 		}
-		h++;
-	}
-	for (h = 10 ; h < 24 ; h++)
-	{
-		int s = 0;
-
-		for (; s < 60 ; s++)
+		else
 		{
-			if (s < 10)
+			int s = 0;
+
+			for (; s < 60 ; s++)
 			{
-				_putchar('0' + h);
-				_putchar(':');
-				_putchar('0');
-				_putchar('0' + s);
-				_putchar('\n');
+				if (s < 10)
+				{
+					_putchar('0' + h);
+					_putchar(':');
+					_putchar('0');
+					_putchar('0' + s);
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar('0' + h);
+					_putchar(':');
+					_putchar('0' + s);
+					_putchar('\n');
+				}
 			}
-			else
-				_putchar('0' + h);
-			_putchar(':');
-			_putchar('0' + s);
-			_putchar('\n');
 		}
 	}
 }
