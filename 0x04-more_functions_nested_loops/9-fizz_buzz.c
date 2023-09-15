1024c2bad5219c79c5213dfb19a29c4a98f637ff
Hamza <hamzabouhouch273@gmail.com>
2023-09-15 11:57:44 -0700

    t

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - prints the numbers from 1 to 100.
+ *
+ * Return: always 0 (Success)
+ *
+*/
+
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1 ; i < 101 ; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else
+			_putchar('0' + i);
+		_putchar(' ');
+	}
+}
