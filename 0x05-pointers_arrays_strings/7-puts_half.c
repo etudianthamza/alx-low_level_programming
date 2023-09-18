49ed9e22fdb73867463b8bd3c473399cf00f952c
hamza <hamzabouhouch273@gmail.com>
2023-09-19 00:47:43 +0100

    h

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line
+ *
+ * @str : string
+ *
+ * Return: Always 0 (Success)
+*/
+
+void puts_half(char *str)
+{
+
