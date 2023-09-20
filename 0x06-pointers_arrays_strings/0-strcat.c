39b6cd0b0fde9777b308a64eb8d8ce96c31220f0
hamza <hamzabouhouch273@gmail.com>
2023-09-20 23:35:05 +0100

    g

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -15,12 +15,12 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0 ; dest[i] != '\0' ; i++)
 	{
-		_putchar(dest[i]);
 	}
 	for (j = 0 ; src[j] != '\0' ; j++)
 	{
-		_putchar(src[j]);
+		dest[i + j] = src[j];
 	}
-	_putchar('\0');
-	return (*dest);
+	dest[i + j] = '\0';
+
+	return (dest);
 }
