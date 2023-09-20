0bc7fb4cb68c679818e3a20090183f2eead99fd6
hamza <hamzabouhouch273@gmail.com>
2023-09-20 23:14:12 +0100

    h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *
+ * _strcat - concatenates two strings
+ * @dest : the first string to concatenates
+ * @src : the second string to concatenates
+ *
+ * Return:Always 0 (Success)
+ *
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int i,j;
+
+	for (i = 0 ; *dest[i] != '\0' ; i++)
+	{
+		_putchar(dest[i]);
+	}
+	for (j = 0 ; *src[i] != '\0' ; j++)
+	{
+		_putchar(src[j]);
+	}
+	_putchar('\0');
+	_putchar(NULL);
+	return (*dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+In this directory , I will discover more of Pointers, Array and String in C programming.
