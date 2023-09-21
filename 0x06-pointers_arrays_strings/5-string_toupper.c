2737c490dcb034f1b5b5bd24a87ea8ec5ccf49c6
hamza <hamzabouhouch273@gmail.com>
2023-09-22 00:27:09 +0100

    h

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,4 +2,18 @@
 
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
- * @
+ * @t: the string on lowercase to UPPERCASE
+ *
+ * Return:Always 0 (Success)
+*/
+
+char *string_toupper(char *t)
+{
+	int i;
+
+	for (i = 0; t[i] != '\0' ; i++)
+	{
+		t[i] = t[i] + 32;
+	}
+	return (t);
+}
