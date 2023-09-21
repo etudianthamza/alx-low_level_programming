6495f30e4b7ee36adf6d2d3f2288e43abcd962a0
hamza <hamzabouhouch273@gmail.com>
2023-09-21 01:50:04 +0100

    h

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncpy - a function that copies a string.
+ *
+ *@dest : the first string
+ *@src : the seconf string
+ *@n : the number of bytes
+ *
+ * Return:Always 0 (Success)
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+	{
+	}
+	for (j = 0 ; src[j] != '\0' && j < n ; j++)
+	{
+		if (j == sizeof(src))
+		{
+			dest = src;
+		}
+		else
+		{
+			dest[j] = src[j];
+		}
+	}
+}
