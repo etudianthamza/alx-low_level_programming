ac144203a6508edea802f2d4c5a4d057aaa6ba7e
hamza <hamzabouhouch273@gmail.com>
2023-09-22 01:38:40 +0100

    u

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -27,7 +27,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-int isDelimiter(char c);
-int islower(char c);
 
 #endif
