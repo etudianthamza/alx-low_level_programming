8640a1b2903a413e271c2cd3d5225f00b41a22a2
hamza <hamzabouhouch273@gmail.com>
2023-09-26 00:05:45 +0100

    h

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard -  prints the chessboard
+ * @a : the area
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; j < 0 ; i++)
+	{
+		for (i = 0; j < 0 ; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
