36c80db86bcb276d039d1c9157dd0041a5202e5b
hamza <hamzabouhouch273@gmail.com>
2023-09-26 00:12:41 +0100

    j

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * @a : the integer
+ * @size : the inetegr size
+ *
+ * Return: Always 0 (Success)
+ *
+*/
+
+void print_diagsums(int *a, int size)
+{
+
