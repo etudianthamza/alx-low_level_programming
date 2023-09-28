135d28ff99741511f33dd95768359ed761d5908c
hamza <hamzabouhouch273@gmail.com>
2023-09-28 16:19:28 +0100

    h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,14 +11,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(s[i]);
-	i++;
-	_puts_recursion(&s[i]);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
