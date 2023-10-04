9893e759ce5a7710afc933ed70ba3d00962d87ea
hamza <hamzabouhouch273@gmail.com>
2023-10-04 10:30:13 +0100

    r

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Print 1 if the alphabet is uppercase , otherwise print 0.
+ *
+ * @c:Enter the alphabet uppercase or other things
+ *
+ * Return: Alawys 0 (Succes)
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
