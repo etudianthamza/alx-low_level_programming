d87c12d64fab9189660f93f4e278a39f0e6d10b6
hamza <hamzabouhouch273@gmail.com>
2023-10-04 13:51:06 +0100

    g

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - function that multiplies two numbers
+ * @argc : the integer
+ * @argv : the characetr
+ * Return: Always 0 (Success)
+ *
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 3)
+	{
+		i = argv[argc - 2] * arg[argc - 1];
+		printf("%d\n", i);
+	}
+	else
+		print("Error\n");
+}
