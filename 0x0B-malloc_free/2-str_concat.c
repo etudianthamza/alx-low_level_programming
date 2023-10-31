dd32ccd4645cda32112b4950956d42e49716cfeb
hamza <hamzabouhouch273@gmail.com>
2023-10-31 22:10:22 +0100

    g

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: the first string
+ * @s2: the second string
+ * Return: Always 0 (Success)
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int i = 0, j = 0, h = 0;
+	char *m;
+
+	if (s1 == NULL)
+		return (NULL);
+	for (; s1[i] != '\0'; i++)
+	;
+	for (; s2[j] != '\0' ; j++)
+	;
+	m = malloc(i * sizeof(*s1) + 1);
+
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; h < i; h++)
+			m[h] = s1[h];
+	}
+	m = realloc(m, (i + j) * sizeof(*s2) + 1);
+
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (h = i + 1; h < (i + j); h++)
+			m[h] = s2[h];
+	}
+	return (m);
+}
+
+
+
+
