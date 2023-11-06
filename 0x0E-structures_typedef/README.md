101abe37bb22e9f5335e28c97d511e29c84c4b7d
hamza <hamzabouhouch273@gmail.com>
2023-11-06 22:17:43 +0100

    g

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+In this derectory, I will do the Sturcture and Typdef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for the dog.
+ * @name: type character.
+ * @age: type age.
+ * @owner: type character.
+ * Return: Always 0 (Success).
+*/
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+#endif
