ea189cfd62954c73807b5bcc7b143226c0d116ad
hamza <hamzabouhouch273@gmail.com>
2023-11-07 00:25:38 +0100

    r

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * new_dog -  function that creates a new dog.
+ * @name: the name of a dog.
+ * @age: he's age.
+ * @owner: he's or she's owner.
+ * Return: Always 0 (Success).
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog
+		if (my_dog)
+		{
+			my_dog->name = name;
+			my_dog->age = age;
+			my_dog->owner = owner;
+			return (&my_dog);
+		}
+		else
+			return ('\0');
+}
+
+
