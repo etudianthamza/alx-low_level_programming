1959567463038f76cd8be633dd2bf172a4891903
hamza <hamzabouhouch273@gmail.com>
2023-11-08 21:29:04 +0100

    e

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: the character wish is the name.
+ * @f: the pointer to function.
+ * Return: Always 0 (Success).
+ *
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(char *name);
+	return (0);
+}
