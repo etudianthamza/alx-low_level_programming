8c97d45c80d7b9455841d17afa261732ae74a055
hamza <hamzabouhouch273@gmail.com>
2023-11-09 22:27:54 +0100

    f

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,7 +2,7 @@
 
 /**
  * sum_them_all - the function returns the sum of all its parameters.
- * @n: the constante integer.
+ * @n: number of arrguments.
  *
  * Return: Always 0 (Success).
 */
@@ -18,7 +18,7 @@ int sum_them_all(const unsigned int n, ...)
 		va_start(ptr, n);
 		for (i = 0; i < n ; i++)
 		{
-			sum += va_arg(ptr, unsigned int);
+			sum += va_arg(ptr, int);
 			va_end(ptr);
 			return (sum);
 		}
