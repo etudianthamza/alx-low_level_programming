298d53d5a853a629854a31051db8b6c13b44b468
hamza <hamzabouhouch273@gmail.com>
2023-11-09 23:28:42 +0100

    e

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,11 +9,11 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	if (separator)
+	if (separator != NULL)
 	{
 		unsigned int i = 0;
 		va_list ptr;
-		
+
 		va_start(ptr, n);
 		for (; i < n - 1; i++)
 		{
