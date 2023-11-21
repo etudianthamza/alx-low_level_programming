3025a71098343001943d5b87281db01d6f3d29e0
hamza <hamzabouhouch273@gmail.com>
2023-11-21 23:50:56 +0100

    update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,12 +8,12 @@
 
 size_t listint_len(const listint_t *h)
 {
-	size_t node_of_list = 0;
+	size_t node = 0;
 
 	while (h)
 	{
 		node++;
 		h = h->next;
 	}
-	return (node_of_list);
+	return (node);
 }
